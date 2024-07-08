8e8648d72a3953ad80818b7da6fbe1203a9b81bb
Berk <161392045+berkborazan@users.noreply.github.com>
2024-07-08 03:23:53 +0300

    Adding more functionality

--- letterboxd-rating-remover.js
+++ letterboxd-rating-remover.js
@@ -1,9 +1,11 @@
 // ==UserScript==
-// @name        Letterboxd Rating Remover - Berk
+// @name        Letterboxd Rating Remover
 // @namespace   https://github.com/berkborazan/letterboxdratingremover
 // @description Removes ratings from film pages
 // @homepageURL https://github.com/berkborazan/letterboxdratingremover
+// @include     *://letterboxd.com/*
 // @include     *://letterboxd.com/film/*
+// @include     *://letterboxd.com/films/*
 // @include     *://letterboxd.com/film/*/crew/*
 // @include     *://letterboxd.com/film/*/studios/*
 // @include     *://letterboxd.com/film/*/genres/*
@@ -21,19 +23,90 @@
 (function() {
     'use strict';
 
-    // Function to hide the element
+    // Hides film ratings from the films' pages
     function hideRatingsChart() {
         const element = document.querySelector('.section.ratings-histogram-chart');
         if (element) {
             element.style.display = 'none';
         }
     }
+		//If you want to remove this function, delete between the comments.
+  
+  
+  	// Hides news sections from the homepage
+    function hideNews() {
+        const elements = document.querySelectorAll('#latest-news');
+        elements.forEach(element => {
+            element.style.display = 'none';
+        });
+    }
+  	//If you want to remove this function, delete between the comments.
 
-    // Initial check
+  
+  	// Hides the stories section from the homepage
+    function hideStories() {
+        const element = document.querySelector('.section.stories-section');
+        if (element) {
+            element.style.display = 'none';
+        }
+    }
+  	//If you want to remove this function, delete between the comments.
+
+  
+    // Hides ratings given by your friends for the films you have not watched yet
+    function removePosterViewingData() {
+        const elements = document.querySelectorAll('.poster-viewingdata');
+        elements.forEach(element => {
+            const parent = element.closest('.poster-container.viewing-poster-container.film-not-watched');
+            if (parent) {
+                element.remove();
+            }
+        });
+    }
+  	//If you want to remove this function, delete between the comments.
+
+  
+  	// Hides the stories section from the homepage
+    function hideProAds() {
+        const element = document.querySelector('.banner.banner-950.js-hide-in-app');
+        if (element) {
+            element.style.display = 'none';
+        }
+    }
+  	//If you want to remove this function, delete between the comments.
+
+
+  	// Hides the average ratings /films/popular overlay. This currently also removes the titles of movies alongside with ratings, will be fixed later.
+    function hideAverageRating() {
+        const elements = document.querySelectorAll('.frame.has-menu');
+        elements.forEach(element => {
+            element.removeAttribute('data-original-title')
+        });
+    }
+  	//If you want to remove this function, delete between the comments.
+
+
+    // Initial checks
     hideRatingsChart();
+    removePosterViewingData();
+    hideNews();
+    hideStories();
+    hideProAds();
+    hideAverageRating();
+
+
 
     // Create a MutationObserver to watch for changes in the DOM
-    const observer = new MutationObserver(hideRatingsChart);
+    const observer = new MutationObserver(() => {
+        hideRatingsChart();
+        removePosterViewingData();
+        hideNews();
+        hideStories();
+        hideProAds();
+        hideAverageRating();
+
+
+    });
 
     // Start observing the document
     observer.observe(document.body, { childList: true, subtree: true });
