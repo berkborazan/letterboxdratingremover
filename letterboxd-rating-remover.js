78665d5af12c27423c2ab6f4e5973508a6d00bce
Berk <161392045+berkborazan@users.noreply.github.com>
2025-05-19 22:41:10 +0200

    Update letterboxd-rating-remover.js
    hideFilmNewsSection();
    hideWheretoWatch();
    hideMentionedBySection();
    removeAttributionClassForBigFilmCounts();
    hideNanoCrowd();
    hideReportFlag();

--- letterboxd-rating-remover.js
+++ letterboxd-rating-remover.js
@@ -32,6 +32,8 @@
     }
 		//If you want to remove this function, delete between the comments.
 
+
+
   	// Hides news sections from the homepage
     function hideNews() {
         const elements = document.querySelectorAll('#latest-news');
@@ -64,6 +66,7 @@
     }
   	//If you want to remove this function, delete between the comments.
 
+
   	// Hides the stories section from the homepage
     function hideProAds() {
         const element = document.querySelector('.banner.banner-950.js-hide-in-app');
@@ -72,6 +75,7 @@
         }
     }
   	//If you want to remove this function, delete between the comments.
+
 
   	// Hides the average ratings /films/popular overlay. This currently also removes the titles of movies alongside with ratings, will be fixed later.
     function hideAverageRating() {
@@ -102,7 +106,6 @@
   	//If you want to remove this function, delete between the comments.
 
 
-	// Hides the Main Showdowns title
     function hideShowdownsSection() {
         const allSections = document.querySelectorAll('section.section');
         allSections.forEach(section => {
@@ -112,9 +115,8 @@
             }
         });
 }
-  	//If you want to remove this function, delete between the comments.
 
-	
+
       	// Hides the Members Title.
     function hideMembers() {
         const element = document.querySelector('.navitem.main-nav-people');
@@ -124,7 +126,7 @@
     }
   	//If you want to remove this function, delete between the comments.
 
-        	// Hides the Journal Title.
+        	// Hides the Members Title.
     function hideJournal() {
         const element = document.querySelector('.navitem.main-nav-journal');
         if (element) {
@@ -143,6 +145,83 @@
     }
   	//If you want to remove this function, delete between the comments.
 
+
+          	// Hides the Film News Section in the Film's Page
+    function hideFilmNewsSection() {
+        const sections = document.querySelectorAll('section.section-margin.film-news');
+
+        sections.forEach(section => {
+            section.style.display = 'none';
+        });
+    }
+
+    	//If you want to remove this function, delete between the comments.
+
+
+            	// Hides the Where to Watch Section in the Film's Page
+    function hideWheretoWatch() {
+      const sections = document.querySelectorAll('section.watch-panel.js-watch-panel');
+
+      sections.forEach(section => {
+          section.style.display = 'none';
+      });
+  }
+    	//If you want to remove this function, delete between the comments.
+
+
+              	// Hides the Where to Watch Section in the Film's Page
+    function hideMentionedBySection() {
+    const section = document.getElementById('film-hq-mentions');
+    if (section) {
+        section.style.display = 'none';
+    }
+}
+    	//If you want to remove this function, delete between the comments.
+
+
+                	// Hides the Lists with more than 200 films in the Film's Page
+    function removeAttributionClassForBigFilmCounts() {
+    const values = document.querySelectorAll('section.list small.value');
+
+    values.forEach(el => {
+        const text = el.textContent.trim();
+        if (text.endsWith('films')) {
+            const number = parseInt(text.replace(/[^\d]/g, ''), 10);
+            if (number > 200) {
+                const section = el.closest('section.list');
+                if (section) {
+                    section.remove();
+                }
+            }
+        }
+    });
+}
+      	//If you want to remove this function, delete between the comments.
+
+
+                  // Hides the NanoCrowd Attribution in the Film's Page
+      function hideNanoCrowd() {
+        const sections = document.querySelectorAll('.nanocrowd-attribution.-is-not-stacked');
+
+        sections.forEach(section => {
+            section.style.display = 'none';
+        });
+    }
+        //If you want to remove this function, delete between the comments.
+
+
+                    // Hides the off-center report flag in the Film's Page
+      function hideReportFlag() {
+        const sections = document.querySelectorAll('.block-flag-wrapper.show-on-hover');
+
+        sections.forEach(section => {
+            section.style.display = 'none';
+        });
+    }
+        //If you want to remove this function, delete between the comments.
+
+
+
     // Initial checks
     hideRatingsChart();
     removePosterViewingData();
@@ -156,6 +235,14 @@
     hideMembers();
     hideJournal();
     hideShowdownsSection();
+    hideFilmNewsSection();
+    hideWheretoWatch();
+    hideMentionedBySection();
+    removeAttributionClassForBigFilmCounts();
+    hideNanoCrowd();
+    hideReportFlag();
+
+
 
     // Create a MutationObserver to watch for changes in the DOM
     const observer = new MutationObserver(() => {
@@ -171,6 +258,14 @@
         hideMembers();
         hideJournal();
         hideShowdownsSection();
+        hideFilmNewsSection();
+        hideWheretoWatch();
+        hideMentionedBySection();
+        removeAttributionClassForBigFilmCounts();
+        hideNanoCrowd();
+        hideReportFlag();
+
+
     });
 
     // Start observing the document
